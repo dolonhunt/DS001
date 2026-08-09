30e52855777b44121b0251b542b9d371e363edac
Dolon Hunt <95109852+dolonhunt@users.noreply.github.com>
2026-08-10 02:43:42 +0600

    Modify useBudget to return context instead of throwing
    Change error throwing to return context as BudgetContextType.

--- src/contexts/BudgetContext.tsx
+++ src/contexts/BudgetContext.tsx
@@ -35,7 +35,7 @@ const BudgetContext = createContext<BudgetContextType | undefined>(undefined);
 
 export function useBudget() {
   const context = useContext(BudgetContext);
-  if (context === undefined) throw new Error('useBudget must be used within BudgetProvider');
+  if (context === undefined) return context as BudgetContextType;
   return context;
 }
 
